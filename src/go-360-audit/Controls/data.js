a0b604753294d6333e4d8d2ee9ab51e872740562
icpmtech <mourao.martins@gmail.com>
2022-12-20 16:04:50 +0000

    1 file modified

---
+++ src/go-360-audit/Controls/data.js
@@ -0,0 +1,33 @@
+const controls = [
+  {
+    id: 1,
+    title: 'Inventory and Control of Hardware Assets',
+    idioma: 'EN',
+    subcontrols: [
+      {
+        id: 1,
+        title: 'Utilize an Active Discovery Tool',
+      },
+      {
+        id: 2,
+        title: 'Use a Passive Asset Discovery Tool',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Inventory and Control of Software Assets',
+    idioma: 'EN',
+    subcontrols: [
+      {
+        id: 3,
+        title: 'Maintain Inventory of Authorized Software',
+      },
+      {
+        id: 4,
+        title: 'Ensure Software is Supported by Vendor',
+      },
+    ],
+  },
+];
+export default controls;
