458f5743cc6d07182ae02a2ea59148a02dcac8f6
PauloPortela2000 <112386127+PauloPortela2000@users.noreply.github.com>
2022-12-21 10:01:06 +0000

    Update data.js

--- src/go-360-audit/Norms/data.js
+++ src/go-360-audit/Norms/data.js
@@ -1,43 +1,79 @@
-const controls = [
+const norms = [
   {
     id: 1,
-    title: 'Norma1',
+    title: 'CIS Controls',
     subcontrols: [
       {
-        id: 1,
-        title: 'Controlo1',
+        id: '1.1',
+        title: 'Inventory and Control of Hardware Assets',
+        idioma: 'EN',
         subcontrols: [
           {
-            id: 1,
-            title: 'Sub-Controlo1',
+            id: '1.1.1',
+            title: 'Utilize an Active Discovery Tool',
+            idioma: 'EN',
           },
-
           {
-            id: 2,
-            title: 'Sub-Controlo2',
+            id: '1.1.2',
+            title: 'Use a Passive Asset Discovery Tool',
+            idioma: 'EN',
+          },
+        ],
+      },
+      {
+        id: '1.2',
+        title: 'Continuous Vulnerability Management',
+        idioma: 'EN',
+        subcontrols: [
+          {
+            id: '1.2.1',
+            title: 'Maintain Inventory of Authorized Software',
+            idioma: 'EN',
+          },
+          {
+            id: '1.2.2',
+            title: 'Ensure Software is Supported by Vendor',
+            idioma: 'EN',
           },
         ],
       },
     ],
   },
-
   {
     id: 2,
-    title: 'Norma2',
+    title: 'ISO 27001',
     subcontrols: [
       {
-        id: 1,
-
-        title: 'Controlo1',
+        id: '2.1',
+        title: 'Inventory and Control of Hardware Assets',
+        idioma: 'EN',
         subcontrols: [
           {
-            id: 1,
-            title: 'Sub-Controlo1',
+            id: '2.1.1',
+            title: 'Utilize an Active Discovery Tool',
+            idioma: 'EN',
           },
-
           {
-            id: 2,
-            title: 'Sub-Controlo2',
+            id: '2.1.2',
+            title: 'Use a Passive Asset Discovery Tool',
+            idioma: 'EN',
+          },
+        ],
+      },
+      {
+        id: '2.2',
+        title: 'Continuous Vulnerability Management',
+        idioma: 'EN',
+        subcontrols: [
+          {
+            id: '2.2.1',
+            title: 'Maintain Inventory of Authorized Software',
+            idioma: 'EN',
+          },
+          {
+            id: '2.2.2',
+            title: 'Ensure Software is Supported by Vendor',
+            idioma: 'EN',
           },
         ],
       },
@@ -45,4 +81,5 @@ const controls = [
   },
 ];
 
-export default controls;
+export default norms;
+
