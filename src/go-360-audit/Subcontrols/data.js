4c507d7d1a51062cea6b81d6bf42eb4c9af84556
PauloPortela2000 <112386127+PauloPortela2000@users.noreply.github.com>
2022-12-21 09:57:31 +0000

    Update data.js

--- src/go-360-audit/Subcontrols/data.js
+++ src/go-360-audit/Subcontrols/data.js
@@ -1,18 +1,19 @@
-const controls = [
+const subcontrols = [
   {
     id: 1,
     title: 'Utilize an Active Discovery Tool',
-    Language: 'EN',
+    idioma: 'EN',
   },
   {
     id: 2,
     title: 'Use a Passive Asset Discovery Tool',
-    Language: 'EN',
+    idioma: 'EN',
   },
   {
     id: 3,
     title: 'Use DHCP Logging to Update Asset Inventory',
+    idioma: 'EN',
   },
 ];
 
-export default controls;
+export default subcontrols;
